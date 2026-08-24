25c53920d290b730edd8e2c4e836a2ff3a07f7c5
Manu <emmanuelbomb17@gmail.com>
2026-08-24 21:57:08 +0300

    Create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>About CommunityHub</h3>
+      <p>
+        CommunityHub is a vibrant platform for connecting with like-minded individuals,
+        sharing ideas, and building meaningful relationships within our community.
+      </p>
+    </aside>
+  );
+}
