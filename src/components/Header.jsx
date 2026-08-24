547ea8a2e21084a7a1c95e3fcb6a3bb5069fbb5c
Manu <emmanuelbomb17@gmail.com>
2026-08-24 21:56:34 +0300

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>CommunityHub</h1>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#posts">Posts</a>
+        <a href="#about">About</a>
+      </nav>
+    </header>
+  );
+}
