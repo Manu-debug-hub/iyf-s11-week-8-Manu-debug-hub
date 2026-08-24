eef3d8ff488094a00c2dee26deae920159677d71
Manu <emmanuelbomb17@gmail.com>
2026-08-24 21:58:36 +0300

    Create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ children, title }) {
+  return (
+    <div className="card">
+      {title && <h3 className="card-title">{title}</h3>}
+      <div className="card-body">{children}</div>
+    </div>
+  );
+}
